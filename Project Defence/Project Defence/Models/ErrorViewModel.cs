3f9ba498a8afa95c0c75e6b2bffd4cc57faf1d90
kafedarov <128903549+kafedarov@users.noreply.github.com>
2024-04-14 23:50:36 +0200

    Delete Project Defence directory

--- Project Defence/Project Defence/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_Defence.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
