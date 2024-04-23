c2c34bb6ffb5a9b77f412c0d1f51d6dfc0d4a9ed
kafedarov <128903549+kafedarov@users.noreply.github.com>
2024-04-23 16:18:35 +0200

    Custom views for error pages

---
+++ BookLibrary/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+	public class ErrorController : Controller
+	{
+        [Route("Error/404")]
+        public IActionResult Error404()
+        {
+            return View("NotFound");
+        }
+
+        [Route("Error/500")]
+        public IActionResult Error500()
+        {
+            return View("InternalError");
+        }
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return View("NotFound");
+                case 500:
+                    return View("InternalError");
+                default:
+                    return View("GenericError");
+            }
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+
+        public IActionResult InternalError()
+        {
+            return View();
+        }
+    }
+}
--- BookLibrary/Program.cs
+++ BookLibrary/Program.cs
@@ -34,12 +34,17 @@ builder.Services.AddScoped<ICartService, CartService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseDeveloperExceptionPage();
+//}
+//else
+//{
+    app.UseExceptionHandler("/Home/Error");
+    // Handle specific status codes with re-execution to custom paths
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
+    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
